405dfe5505b519567b0ce30e01d4293d4ee98e41
Joraj J R <jorajmsc@yahoo.com>
2024-12-16 15:49:31 +0530

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { ProjectsComponent } from './projects/projects.component';
+import { VisionComponent } from './vision/vision.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'vision', component: VisionComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
